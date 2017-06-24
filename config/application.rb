4c839ef4d63d3506b14e6941e0f038bf0d2191e3
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2017-06-24 13:11:30 +0900

    display top page

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module EkispertRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # use slim engine
+    config.generators.template_engine = :slim
   end
 end
